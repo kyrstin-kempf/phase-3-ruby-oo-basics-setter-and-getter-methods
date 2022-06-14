046d09dd8cfd3abb40ce9858a096535b6330053f
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2022-06-14 12:37:36 -0400

    commit tree 52367b50169bb87c6971fef47d7d09fb8f68f733

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+# GETTER ------------
+def name
+    @name
+end
+
+def breed
+    @breed
+end
+
+# SETTER ------------
+def name=(name)
+    @name = name
+end
+
+def breed=(breed)
+    @breed = breed
+end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+# GETTER METHOD -----------------
+def name
+    @name
+end
+
+def job
+    @job
+end
+
+# SETTER METHOD -----------------
+def name=(name)
+    @name = name
+end
+
+def job=(job)
+    @job = job
+end
 
 end
